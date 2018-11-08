d72a5c53ab2ca574b34a998208c2570800deecbe
Andre <amadry@masonlive.gmu.edu>
2018-11-08 17:38:14 -0500

    5

---
+++ main.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    // run function on initial page load
+    demoFunction();
+    mouseEvent();
+    // run function on resize of the window
+    $(window).resize(function() {
+       
+    });
+    // run function on scroll
+    $(window).scroll(function() {
+ 
+    });
+});
+
+
+function interactions() {
+	$('jqClick').click
+}
