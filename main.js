e646005ca68f7635c42a356bf4768c92c9891052
Andre <amadry@masonlive.gmu.edu>
2018-11-13 11:22:26 -0500

    push

--- main.js
+++ main.js
@@ -1,18 +1,29 @@
 $(document).ready(function() {
     // run function on initial page load
-    demoFunction();
-    mouseEvent();
+    interactions();
+    variables();
+ 
     // run function on resize of the window
     $(window).resize(function() {
-       
+   
     });
+ 
     // run function on scroll
     $(window).scroll(function() {
  
     });
 });
 
+function interactions() {
+    $('#number5').click(function(){
+        $('.box').toggleClass('GreenBG');
+        console.log('clicked');
+    });
+}
 
 function interactions() {
-	$('jqClick').click
+    $('#number2').click(function(){
+        $('.Mammal').toggleClass('');
+        console.log('hovered');
+    });
 }
